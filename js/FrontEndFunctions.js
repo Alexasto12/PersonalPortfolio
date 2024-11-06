213727369a1b27e90768ec09a6e556a83a9e50bd
Alejandro-Cabrera-Carrasco <Alexasto2000@gmail.com>
2024-11-06 21:20:44 +0100

    Style modified, Js too, added video background

--- js/FrontEndFunctions.js
+++ js/FrontEndFunctions.js
@@ -1,9 +1,12 @@
 function toggleStyleButton() {
+    const videoElement = document.getElementById('myVideo');
     if (document.documentElement.classList.contains('light')) {
-        document.documentElement.classList.replace('light','dark'); // Amb el replace es canvia el valor de la classe light per dark (Arg: token1, newtoken)
-    }else{
-        document.documentElement.classList.replace('dark','light');
+        document.documentElement.classList.replace('light', 'dark'); // Canvia la classe de light a dark
+        videoElement.src = './media/dark-background.mp4'; // Canvia el src del vídeo a dark
+        videoElement.playbackRate = 0.75; // Ajusta la velocitat de reproducció per al mode fosc
+    } else {
+        document.documentElement.classList.replace('dark', 'light'); // Canvia la classe de dark a light
+        videoElement.src = './media/light-background.mp4'; // Canvia el src del vídeo a light
+        videoElement.playbackRate = 0.75; // Ajusta la velocitat de reproducció per al mode clar
     }
-    
-    
 }
