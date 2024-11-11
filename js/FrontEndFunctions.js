6fedff96a49fba40ca8fc781c1f01b1a6207a173
Alejandro-Cabrera-Carrasco <Alexasto2000@gmail.com>
2024-11-11 17:06:11 +0100

    Changed background from .mp4 to JSON using loitte libray, this branch is for nav menu, but this is important in order to try interactions with nav and animations in the back ground.

--- js/FrontEndFunctions.js
+++ js/FrontEndFunctions.js
@@ -4,9 +4,26 @@ function toggleStyleButton() {
         document.documentElement.classList.replace('light', 'dark'); // Canvia la classe de light a dark
         videoElement.src = './media/dark-background.mp4'; // Canvia el src del vídeo a dark
         videoElement.playbackRate = 0.75; // Ajusta la velocitat de reproducció per al mode fosc
+        lottieAnimation('./media/JSON/Dark-Background.json'); // Carrega l'animació per al mode fosc
     } else {
         document.documentElement.classList.replace('dark', 'light'); // Canvia la classe de dark a light
         videoElement.src = './media/light-background.mp4'; // Canvia el src del vídeo a light
-        videoElement.playbackRate = 0.75; // Ajusta la velocitat de reproducció per al mode clar
+        videoElement.playbackRate = 1.0; // Ajusta la velocitat de reproducció per al mode clar
+        lottieAnimation('./media/JSON/Light-Background.json'); // Carrega l'animació per al mode clar
     }
 }
+
+function lottieAnimation(path) {
+    lottie.loadAnimation({
+        container: document.getElementById('lottie-container'), // Contenedor para la animación
+        renderer: 'svg', // Renderiza como SVG para alta calidad
+        loop: true, // Activa el loop para que se repita continuamente
+        autoplay: true, // Reproduce automáticamente
+        path: path // Ruta al archivo JSON de la animación
+    });
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    lottieAnimation('./media/JSON/Dark-Background.json');
+    lottieAnimation('./media/JSON/Light-Background.json') // Carrega l'animació per defecte
+});
