27a46c5ade27a816a2b058651808e48ee1892c84
Alejandro-Cabrera-Carrasco <Alexasto2000@gmail.com>
2024-11-13 20:41:00 +0100

    Header almost finished, need to complete the body in order to interact with body elements

--- js/FrontEndFunctions.js
+++ js/FrontEndFunctions.js
@@ -1,29 +1,38 @@
+let currentAnimation = null;
+
 function toggleStyleButton() {
-    const videoElement = document.getElementById('myVideo');
+   
     if (document.documentElement.classList.contains('light')) {
-        document.documentElement.classList.replace('light', 'dark'); // Canvia la classe de light a dark
-        videoElement.src = './media/dark-background.mp4'; // Canvia el src del vídeo a dark
-        videoElement.playbackRate = 0.75; // Ajusta la velocitat de reproducció per al mode fosc
+        document.documentElement.classList.replace('light', 'dark'); // Canvia la classe de light a dark 
+        setTimeout(() => {
         lottieAnimation('./media/JSON/Dark-Background.json'); // Carrega l'animació per al mode fosc
+        }, 250);
     } else {
         document.documentElement.classList.replace('dark', 'light'); // Canvia la classe de dark a light
-        videoElement.src = './media/light-background.mp4'; // Canvia el src del vídeo a light
-        videoElement.playbackRate = 1.0; // Ajusta la velocitat de reproducció per al mode clar
-        lottieAnimation('./media/JSON/Light-Background.json'); // Carrega l'animació per al mode clar
+        setTimeout(() => {
+
+            lottieAnimation('./media/JSON/Light-Background.json'); // Carrega l'animació per al mode clar
+        }, 250);
     }
 }
 
 function lottieAnimation(path) {
-    lottie.loadAnimation({
+    if (currentAnimation) {
+        currentAnimation.destroy(); // Destrueix l'animació anterior
+    }
+    currentAnimation = lottie.loadAnimation({
         container: document.getElementById('lottie-container'), // Contenedor para la animación
         renderer: 'svg', // Renderiza como SVG para alta calidad
-        loop: true, // Activa el loop para que se repita continuamente
-        autoplay: true, // Reproduce automáticamente
+        loop: true, // Activa el loop para que se repita contínuament
+        autoplay: true, // Reproduce automàticament
         path: path // Ruta al archivo JSON de la animación
     });
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    lottieAnimation('./media/JSON/Dark-Background.json');
-    lottieAnimation('./media/JSON/Light-Background.json') // Carrega l'animació per defecte
+    if (document.documentElement.classList.contains('light')) {
+        lottieAnimation('./media/JSON/Light-Background.json'); // Carrega l'animació per al mode clar
+    } else {
+        lottieAnimation('./media/JSON/Dark-Background.json'); // Carrega l'animació per al mode fosc
+    }
 });
